59afff8f30e4f397ac10352b859c73a34437e9db
utkarsh <utkarshdhsbgp@gmail.com>
2021-04-09 00:11:22 +0530

    Added Button for Web query

--- models/menu.py
+++ models/menu.py
@@ -247,6 +247,23 @@ def _():
                            'http://webchat.freenode.net/?channels=web2py'),
                         ]),
                       ]
+# sistema de consulta web 
+                      from tkinter import *
+import webbrowser
+
+root = Tk()
+
+new = 1
+url = "https://www.google.com"
+
+def openweb():
+    webbrowser.open(url,new=new)
+
+Btn = Button(root, text = "This opens Google",command=openweb)
+Btn.pack()
+
+root.mainloop()
+# end
 if DEVELOPMENT_MENU:
     _()
 
