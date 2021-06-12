90a4e53cc1deb8ce9902c7efadf6c5663a650599
Rodriguez Juan <73370773+Juerodriguez@users.noreply.github.com>
2021-06-12 16:59:45 -0300

    Commit with All suggested changes in script

--- modules/filtro_carga_csv/filtro.py
+++ modules/filtro_carga_csv/filtro.py
@@ -2,25 +2,18 @@ import csv
 import collections
 import argparse
 
-
 data = []
-data_in_column =[]
+data_in_column = []
 
-def filtered(file_csv,ent_column):
-    with open(file_csv, newline='') as File:
-        input_data = csv.reader(File)
-
-        for i, file in enumerate(input_data):
-            if i == 0:
-                column_data = file.index(ent_column)
-                data.append(file[column_data])
-            if i > 0:
-                data.append(file[column_data])
-
+def filtered_csv(file_csv, ent_column):
+    global data
+    global data_in_column
+    with open(file_csv, newline = '') as File:
+        input_data = csv.DictReader(File)
+        for file in input_data:
+            data.append(file[ent_column])
         key_data = list(collections.OrderedDict.fromkeys(data))
-
-        for i,key in enumerate(key_data):
-            data_in_column.append([key])
+        data_in_column = [[row] for row in key_data]
 
 def upload(out_csv):
     with open(out_csv, "w", newline='') as file:
@@ -30,17 +23,15 @@ def upload(out_csv):
 
 def parse_args():
     parser = argparse.ArgumentParser(description = "Este Script sirve para filtrar los csv que contengan una lista de jurisdicciones")
-    parser.add_argument("file_csv", help="Archivo de entrada csv")
-    parser.add_argument("ent_column", help="Nombre de columna de jurisdicciones")
-    parser.add_argument("out_csv", help="Archivo csv de salida")
+    parser.add_argument("file_csv", help = "Archivo de entrada csv")
+    parser.add_argument("ent_column", help = "Nombre de columna de jurisdicciones")
+    parser.add_argument("out_csv", help = "Archivo csv de salida")
     return parser.parse_args()
-
 
 def main():
     args = parse_args()
-    filtered(args.file_csv,args.ent_column)
+    filtered_csv(args.file_csv, args.ent_column)
     upload(args.out_csv)
-
 
 if __name__ == "__main__":
     main()
