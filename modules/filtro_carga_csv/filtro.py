c2c294ca26b9dab4f7d0fe1c82a88e3fa1b280af
Rodriguez Juan <73370773+Juerodriguez@users.noreply.github.com>
2021-06-14 11:22:31 -0300

    Update filtro.py
    Verified PEP8 using flake8

--- modules/filtro_carga_csv/filtro.py
+++ modules/filtro_carga_csv/filtro.py
@@ -2,36 +2,43 @@ import csv
 import collections
 import argparse
 
-data = []
-data_in_column = []
+DATA_IN_COLUMN = []
+
 
 def filtered_csv(file_csv, ent_column):
-    global data
     global data_in_column
-    with open(file_csv, newline = '') as File:
-        input_data = csv.DictReader(File)
-        for file in input_data:
-            data.append(file[ent_column])
+    with open(file_csv, newline='') as file_:
+        input_data = csv.DictReader(file_)
+        data = [file[ent_column] for file in input_data]
         key_data = list(collections.OrderedDict.fromkeys(data))
-        data_in_column = [[row] for row in key_data]
+        DATA_IN_COLUMN = [[row] for row in key_data]
+
 
 def upload(out_csv):
-    with open(out_csv, "w", newline='') as file:
-        cargar = csv.writer(file)
-        cargar.writerows(data_in_column)
-    file.close()
+    with open(out_csv, "w", newline='') as file_:
+        uploading = csv.writer(file_)
+        uploading.writerows(DATA_IN_COLUMN)
+    file_.close()
+
 
 def parse_args():
-    parser = argparse.ArgumentParser(description = "Este Script sirve para filtrar los csv que contengan una lista de jurisdicciones")
-    parser.add_argument("file_csv", help="Archivo de entrada csv")
-    parser.add_argument("ent_column", help="Nombre de columna de jurisdicciones")
-    parser.add_argument("out_csv", help="Archivo csv de salida")
+    parser = argparse.ArgumentParser(
+        description="Este Script filtra los csv que contengan la lista de jurisdicciones")
+    parser.add_argument(
+        "file_csv", help="Archivo de entrada csv")
+    parser.add_argument(
+        "ent_column", help="Nombre de columna de jurisdicciones")
+    parser.add_argument(
+        "out_csv", help="Archivo csv de salida")
     return parser.parse_args()
+
 
 def main():
     args = parse_args()
     filtered_csv(args.file_csv, args.ent_column)
     upload(args.out_csv)
 
+
 if __name__ == "__main__":
     main()
+
