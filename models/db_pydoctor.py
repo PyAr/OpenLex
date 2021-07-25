0ed8d1c8345bab55635a30446135639ca232a99e
Juan Elias Rodriguez <stylorj@gmail.com>
2021-07-25 12:29:10 -0300

    Suggested changes and rename files by issue 44

--- models/db_pydoctor.py
+++ models/db_pydoctor.py
@@ -210,8 +210,7 @@ db.define_table(
         'expediente_id',
         db.expediente,
         label=T('Expediente'),
-        widget=autocomplete_expte_widget
-        ),
+        widget=autocomplete_expte_widget),
     Field(
         'estado',
         length=2,
